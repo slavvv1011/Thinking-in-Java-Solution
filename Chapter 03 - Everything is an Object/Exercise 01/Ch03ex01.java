e0e8e5411663d93b80db632ec6523d7076d39015
slavvv1011 <przemmoskal@gmail.com>
2017-04-05 22:17:56 +0200

    Chapter 03 Exercise 01

---
+++ Chapter 03 - Everything is an Object/Exercise 01/Ch03ex01.java
@@ -0,0 +1,17 @@
+package ch03ex01;
+
+public class Ch03ex01 {
+	int i;
+	char c;
+	public static void main(String[] args) {
+		Ch03ex01 obj = new Ch03ex01();
+		System.out.println("int i = " + obj.i);
+		System.out.println("char c = " + obj.c);
+	}
+
+}
+
+/* Output:
+int i = 0
+char c = 
+*/
