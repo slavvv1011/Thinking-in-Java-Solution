3b565403d7d086089b7d848c637516706535e7f6
slavvv1011 <przemmoskal@gmail.com>
2017-04-06 00:26:18 +0200

    Remove a, b, c folders with Javadocs

--- Chapter 03 - Everything is an Object/Exercise 13/a/src/object/Documentation1.java
+++
@@ -1,9 +0,0 @@
-//: object/Documentation1.java
-package object;
-/** A class comment */
-public class Documentation1 {
-  /** A field comment */
-  public int i;
-  /** A method comment */
-  public void f() {}
-} ///:~
--- Chapter 03 - Everything is an Object/Exercise 13/b/src/object/Documentation2.java
+++
@@ -1,9 +0,0 @@
-//: object/Documentation2.java
-package object;
-/**
-* <pre>
-* System.out.println(new Date());
-* </pre>
-*/
-public class Documentation2 {}
-///:~
