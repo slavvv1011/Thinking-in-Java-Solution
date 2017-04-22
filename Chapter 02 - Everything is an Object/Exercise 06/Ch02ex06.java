dafbd4063a04bc399b7bb37b788191def7590c7c
slavvv1011 <przemmoskal@gmail.com>
2017-04-22 23:50:56 +0200

    Add Ch02Ex06

--- Chapter 02 - Everything is an Object/Exercise 06/Ch02ex06.java
+++ Chapter 02 - Everything is an Object/Exercise 06/Ch02ex06.java
@@ -1,11 +1,11 @@
-package ch03ex06;
+package ch02ex06;
 
-public class Ch03ex06 {
+public class Ch02ex06 {
 	int storage(String s) {
 		return s.length() * 2;
 	}
 	public static void main(String[] args) {
-		Ch03ex06 obj = new Ch03ex06();
+		Ch02ex06 obj = new Ch02ex06();
 		System.out.println(obj.storage("four"));
 	}
 }
