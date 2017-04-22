41eacf7c09c55bb89c0fbd81760fa6c159215d1c
slavvv1011 <przemmoskal@gmail.com>
2017-04-23 01:12:02 +0200

    Chapter 03 Exercise 01

---
+++ Chapter 03 - Operators/Exercise 01/Ch03ex01.java
@@ -0,0 +1,16 @@
+package ch03ex01;
+import static net.mindview.util.Print.*;
+
+public class Ch03ex01 {
+
+	public static void main(String[] args) {
+		System.out.println("System.out.println() used...");
+		print("print() used...");
+	}
+
+}
+
+/* Output:
+System.out.println() used...
+print() used...
+*/
